1d5c768a5299c09e464bd554aef93a0fb77a2e72
CoreyOliver <coreyoliver.dev@gmail.com>
2022-12-06 21:02:35 -0500

    session end

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 canvas.height = window.innerHeight
 canvas.width = window.innerWidth
+const collisionCanvas = document.getElementById('collisionCanvas')
+const collisionCtx = collisionCanvas.getContext('2d')
+collisionCanvas.height = window.innerHeight
+collisionCanvas.width = window.innerWidth
+let score = 0
+ctx.font = '50px Impact'
 
 let timeToNextRaven = 0
 let ravenInterval = 500
 let lastTime = 0
-let gameSpeed = 0
 
 let ravens = []
 class Raven {
@@ -23,10 +28,12 @@ class Raven {
         this.markForDeletion = false
         this.image = new Image()
         this.image.src = 'raven.png'
-        this.frame = 0
+        this.frame = 0  
         this.maxFrame = 4
         this.timeSinceFlap = 0
         this.flapInterval = Math.random() * 50 + 50
+        this.randomColors = [Math.floor(Math.random() * 255),Math.floor(Math.random() * 255),Math.floor(Math.random() * 255)]
+        this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')'
     }
     update(deltaTime){
         if(this.y < 0 || this.y > canvas.height - this.height) {
@@ -43,25 +50,68 @@ class Raven {
         }
     }   
     draw() {
-        ctx.strokeRect(this.x,this.y,this.width,this.height)
+        collisionCtx.fillStyle = this.color
+        collisionCtx.fillRect(this.x,this.y,this.width,this.height)
         ctx.drawImage(this.image,this.frame * this.spriteWidth,0,this.spriteWidth,this.spriteHeight,this.x,this.y,this.width,this.height)
     }
 }
 
+let explosion = []
+class Explosion {
+    constructor(x, y, size) {
+        this.image = new Image()
+        this.image.src = 'boom.png'
+        this.spriteWidth = 200
+        this.spriteHeight = 179
+        this.size = size
+        this.x = x
+        this.y = y
+        this.frame = 0
+        this.sound = new Audio()
+        // this.sound.src = add sound!!!!!!!!!!!
+    }
+}
+function drawScore() {
+    ctx.fillStyle = 'black'
+    ctx.fillText('Score: ' + score, 50, 75)
+    ctx.fillStyle = 'white'
+    ctx.fillText('Score: ' + score, 55, 80)
+
+}
+
+window.addEventListener('click', function(e) {
+    const detectPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1)
+    console.log(detectPixelColor);
+    const pc = detectPixelColor.data
+    ravens.forEach( o => {
+        if(
+        o.randomColors[0] === pc[0] &&
+        o.randomColors[1] === pc[1] &&
+        o.randomColors[2] === pc[2] 
+        ){
+            o.markForDeletion = true
+            score++
+        }
+    })
+})
 
 function animate(timestamp) {
     ctx.clearRect(0,0,canvas.width,canvas.height)
+    collisionCtx.clearRect(0,0,canvas.width,canvas.height)
     let deltaTime = timestamp - lastTime
     lastTime = timestamp
     timeToNextRaven += deltaTime
     if(timeToNextRaven > ravenInterval) {
         ravens.push(new Raven())
         timeToNextRaven = 0
+        ravens.sort(function(a,b){
+            return a.width - b.width
+        })
     }
+    drawScore();
     [...ravens].forEach(object => object.update(deltaTime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(o => !o.markForDeletion)
-    gameSpeed++
     requestAnimationFrame(animate)
 }
-// animate(0)
+animate(0)
